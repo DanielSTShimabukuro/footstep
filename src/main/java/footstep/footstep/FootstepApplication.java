f2af14b17a38b0de5ac5d1f46e499204a253d942
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-17 16:08:55 -0300

    feat: add initial configs

---
+++ src/main/java/footstep/footstep/FootstepApplication.java
@@ -0,0 +1,13 @@
+package footstep.footstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootstepApplication.class, args);
+	}
+
+}
