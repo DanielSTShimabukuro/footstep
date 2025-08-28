807477140eafcb1549dbd663d00bf01c92075436
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2025-08-28 00:02:07 -0300

    feat(ControllerExceptionError): add handleBadCredentialsException function

--- src/main/java/footstep/footstep/configs/security/ControllerExceptionError.java
+++ src/main/java/footstep/footstep/configs/security/ControllerExceptionError.java
@@ -3,6 +3,7 @@ package footstep.footstep.configs.security;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,14 +13,22 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class ControllerExceptionError {
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ExceptionDTO> handleDataIntegrityViolationException (DataIntegrityViolationException exception) {
+    public ResponseEntity<ExceptionDTO> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "400");
 
         return new ResponseEntity<>(exceptionDTO, HttpStatus.BAD_REQUEST);
     
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionDTO> handleBadCredentialsException(BadCredentialsException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "400");
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ExceptionDTO> handleEntityNotFoundException (EntityNotFoundException exception) {
+    public ResponseEntity<ExceptionDTO> handleEntityNotFoundException(EntityNotFoundException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "404");
 
         return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
